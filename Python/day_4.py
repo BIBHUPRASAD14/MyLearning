368d632bdb808812f38f9e7395e52ad0b58dd73b
BIBHUPRASAD14 <bibhuprasadb868@gmail.com>
2024-09-11 00:48:38 +0530

    learning python

---
+++ Python/day_4.py
@@ -0,0 +1,5 @@
+#learning print statements and its syntax
+print("Hello World")
+print(5)
+print("Bye")
+print(17*13)
---
+++ Python/day_5.py
@@ -0,0 +1,21 @@
+''' Author: Bibhuprasad
+ Hey, today is my 5th day of learning python with code with harry
+ Here we will learn- comments, escape sequence, and print statements'''
+
+#escape sequence
+
+# we use backslash for escape sequence
+print("Hello guys,\nWelcome to learn python with\ncode with Harry") # \n for new line
+
+
+print("Hello, I am a \"Good boy\"")      # \ for " in good boy 
+
+# sep="" to separate using someting 
+# end="" to end the print statement with something
+#Example
+print("Hey", 6, 7, sep="~", end="552\n") #this hey ,6 and 7 will be printed separating with ~ symbol and end with something
+print("This is a new line")
+
+
+
+
